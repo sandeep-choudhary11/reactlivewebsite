335e11e9f9595c428d628af4e3dfbec52ee033e1
sandeep-choudhary11 <64455106+sandeep-choudhary11@users.noreply.github.com>
2020-07-17 15:11:42 +0530

    Updates

---
+++ precache-manifest.3fc1d6e2cf3c46bbed1d0859f53a8ac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25c350c6d0760292052f42c99155d1e",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "863534a19ac77a3e94aa",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "d5a5d7af16ed2791976e",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/static/css/main.a2383e98.chunk.css"
+  },
+  {
+    "revision": "863534a19ac77a3e94aa",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/static/js/2.3e0f42a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/static/js/2.3e0f42a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a5d7af16ed2791976e",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/static/js/main.4b53aac8.chunk.js"
+  },
+  {
+    "revision": "511443711d21da5c670e",
+    "url": "/https//sandeep-choudhary11.github.io/reactlivewebsite/static/js/runtime-main.21aac810.js"
+  }
+]);
